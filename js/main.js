674d6465c5eeaf8dd6aa238074b92f36b0a72643
PropeReferio <lemuel.b.stevens@gmail.com>
2020-01-23 13:52:02 -0600

    edits

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //     let year = $("#yearlap input[name=year]").val()
 //     let lap = $("#yearlap input[name=lap]").val()
 //     console.log(year, lap)
-// })  ^ jQuery stuff that Joel couldn't get to work
+// })  ^ jQuery stuff that wouldn't work
 
 // Make two vars for year and lap from form data
  //Change these
@@ -45,8 +45,3 @@ function submitForm() {
 Using Jquery and the DOM you create a table of data using the F1 racer 
 API. The table should have a total of 7 rows and dynamically populate 
 the data when a "year" and "lap" are specified within your form. */
-
-
-//For this assignment, we were not taught how to do JS or jQuery for loops,
-//How to add to an HTML element what we took from the database,
-// And Joel struggled to make it happen himself.
